4eb7481146d4f631ff113525af0d55cf8dd53954
ifLeozao <leozaos1000@gmail.com>
2021-12-22 11:50:27 -0300

    a8

--- src/action.js
+++ src/action.js
@@ -1,10 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fetch = require('node-fetch');
 
+//0
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 
+    const randomPos = Math.round(Math.random() * 100);
+    const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+    const response = await fetch(url);
+    const { results } = await response.json();
+    const gifUrl = results[0].media[0].tinygif.url;
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
     const { context = {} } = github;
